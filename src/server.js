baaa1e99996b38a6cd9d0fd01fd73ce0d0df1720
clara seijo <clasei@proton.me>
2025-01-28 20:11:52 +0100

    new endpoint to receive data from fastAPI

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import fs from "fs";
 import routes from "./routes/index.js";
 
 dotenv.config();
@@ -8,14 +9,30 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5005;
 
-// Middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// Main Routes
+// main Routes
 app.use("/api", routes);
 
-// Start the server
+// new endpoint to receive machine data from FastAPI
+app.post("/upload-data", (req, res) => {
+    try {
+        const machineData = req.body;
+
+        // save data to a local JSON file -- for now
+        fs.writeFileSync("./src/models/machineData.json", JSON.stringify(machineData, null, 2));
+
+        console.log("data received and saved");
+        res.status(200).json({ message: "Data received" });
+    } catch (error) {
+        console.error("error saving data:", error);
+        res.status(500).json({ message: "Failed to save data." });
+    }
+});
+
+// start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
